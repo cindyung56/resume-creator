8f938f294d1d088fd7352dd434ef34197910c6ae
Stevo <steven.barrios92@gmail.com>
2022-09-30 11:32:33 -0700

    changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const resumeRoutes = require('./resume');
 
 
 router.use('/users', userRoutes);
-
+router.use('/resume', resumeRoutes);
 
 module.exports = router;
 
