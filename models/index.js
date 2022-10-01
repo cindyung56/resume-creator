b99895a7b765654e069c4b79a04c254402b0cae4
cindy <cindyung56@gmail.com>
2022-10-01 11:30:21 -0700

    created other models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,10 @@
-// create relationships between Resume and User
+// create relationships between all models
 const User = require("./User");
 const Resume = require('./Resume');
+const Experience = require('./Experience');
+const Education = require('./Education');
+const Reference = require('./Reference');
+const Skill = require('./Skill');
 
 User.hasOne(Resume, {
     foreignKey: 'user_id',
