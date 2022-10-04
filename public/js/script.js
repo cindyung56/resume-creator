8dc201bbac421c51b2bf4357c125792e05aa36ba
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2022-10-04 10:29:41 -0700

    fixed js, changed html homepage, redid cy-resume, and css

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 const addBtn = $("#addExperienceBtn");
+const educationBtn = $("#addEducationBtn");
+const projectsBtn = $('#addProjectsBtn');
 const uploadBtn = $(".btn");
 const experience = $(".experience");
 const education = $(".education");
@@ -14,7 +16,7 @@ addBtn.on("click", function () {
         $("<label>")
           .attr("for", `experienceText${count}`)
           .addClass("form-label")
-          .text("test")
+          .text("Experience")
       )
       .append(
         $("<textarea>")
@@ -25,3 +27,45 @@ addBtn.on("click", function () {
   );
   count++;
 });
+
+educationBtn.on("click", function () {
+    console.log("it clicks");
+    education.append(
+      $("<div>")
+        .addClass("mb-3")
+        .append(
+          $("<label>")
+            .attr("for", `educationText${count}`)
+            .addClass("form-label")
+            .text("Education")
+        )
+        .append(
+          $("<textarea>")
+            .addClass("form-control")
+            .attr("rows", "3")
+            .attr("id", `educationText${count}`)
+        )
+    );
+    count++;
+  });
+
+  projectsBtn.on("click", function () {
+    console.log("it clicks");
+    projects.append(
+      $("<div>")
+        .addClass("mb-3")
+        .append(
+          $("<label>")
+            .attr("for", `projectsText${count}`)
+            .addClass("form-label")
+            .text("Projects")
+        )
+        .append(
+          $("<textarea>")
+            .addClass("form-control")
+            .attr("rows", "3")
+            .attr("id", `projectsText${count}`)
+        )
+    );
+    count++;
+  });
