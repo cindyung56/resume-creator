b58e714b9aa1c78ebc4f89a9e2b35a0c7ac49980
cindy <cindyung56@gmail.com>
2022-10-04 13:17:22 -0700

    added extra inputs

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,25 @@
 const addBtn = $("#addExperienceBtn");
 const educationBtn = $("#addEducationBtn");
 const projectsBtn = $('#addProjectsBtn');
+const referencesBtn = $('#addReferencesBtn');
+const skillsBtn = $('#addSkillsBtn');
 const uploadBtn = $(".btn");
+
 const experience = $(".experience");
 const education = $(".education");
 const projects = $(".projects");
+const references = $(".references");
+const skills = $(".skills");
+
 const tempDis1 = $("#temp1");
 const templateC = $('#cindys-template');
 let count = 1;
+const createButton = $("#createButton");
+
+
 
 addBtn.on("click", function () {
-  console.log("it clicks");
+  // console.log("it clicks");
   experience.append(
     $("<div>")
       .addClass("mb-3")
@@ -32,7 +41,7 @@ addBtn.on("click", function () {
 
 
 educationBtn.on("click", function () {
-    console.log("it clicks");
+    // console.log("it clicks");
     education.append(
       $("<div>")
         .addClass("mb-3")
@@ -52,26 +61,88 @@ educationBtn.on("click", function () {
     count++;
   });
 
-  projectsBtn.on("click", function () {
-    console.log("it clicks");
-    projects.append(
-      $("<div>")
-        .addClass("mb-3")
-        .append(
-          $("<label>")
-            .attr("for", `projectsText${count}`)
-            .addClass("form-label")
-            .text("Projects")
-        )
-        .append(
-          $("<textarea>")
-            .addClass("form-control")
-            .attr("rows", "3")
-            .attr("id", `projectsText${count}`)
-        )
-    );
-    count++;
-  });
+projectsBtn.on("click", function () {
+  // console.log("it clicks");
+  projects.append(
+    $("<div>")
+      .addClass("mb-3")
+      .append(
+        $("<label>")
+          .attr("for", `projectsText${count}`)
+          .addClass("form-label")
+          .text("Projects")
+      )
+      .append(
+        $("<textarea>")
+          .addClass("form-control")
+          .attr("rows", "3")
+          .attr("id", `projectsText${count}`)
+      )
+  );
+  count++;
+});
+
+referencesBtn.on("click", function () {
+  references.append(
+    $('<div>')
+      .addClass("mb-3")
+      .append(
+        $("<h5>").text("Name:")
+      )
+      .append(
+        $("<input>")
+          .attr({
+            class: "form-control form-control-sm referenceName",
+            type: "text",
+            placeholder: "Reference Name",
+            'aria-label': '.form-control-sm example',
+          })
+      )
+      .append($("<h5>").text("Email:"))
+      .append(
+        $("<input>")
+          .attr({
+            class: "form-control form-control-sm referenceEmail",
+            type: "text",
+            placeholder: "Reference Email",
+            'aria-label': '.form-control-sm example',
+          })
+      )
+      .append($("<h5>").text("Phone Number:"))
+      .append(
+        $("<input>")
+          .attr({
+            class: "form-control form-control-sm referencePhone",
+            type: "text",
+            placeholder: "Reference Phone #",
+            'aria-label': '.form-control-sm example',
+          })
+      )
+  )
+});
+
+
+skillsBtn.on('click', function(){
+  skills.append(
+    $("<div>")
+      .addClass("mb-3")
+      .append(
+        $("<input>")
+          .attr({
+            class: "form-control form-control-sm skill",
+            type: "text",
+            placeholder: "Skill",
+            'aria-label': ".form-control-sm example",
+          })
+      )
+  )
+});
+
+
+
+
+
+
 
 // this section is my section camille :D
 
@@ -94,7 +165,7 @@ const templateUrls = [
 // add event listener to the ul 
 // if event . taget = li, then 
 // images-container.filter 
-// if the event . taret . = "id" match then select display
+// if the event . target . = "id" match then select display
 const imagesContainer= $('.images-container')
 
 $('ul').on("click", "button", (e) => {
@@ -109,3 +180,33 @@ $('ul').on("click", "button", (e) => {
   } 
 )
 
+// create button and send information to the back-end
+createButton.on('click', function (){
+  const resumeObject = {
+    "name": $('#resumeName').val(),
+    "email": $('#resumeEmail').val(),
+    "phone": $('#resumePhone').val(),
+    "linkedin": $('#linkedinLink').val(),
+    "portfolio": $('#portfolioLink').val(),
+    "title": $('#jobTitle').val(),
+  };
+
+  let experienceArray = [];
+  let educationArray = [];
+
+  let referencesArray = [];
+  
+
+
+  let skillArray = [];
+  for (let i = 0; i < $('.skill').length; i++){
+    const skillInput = $($('.skill')[i]).val();
+    skillArray.push({"name": skillInput});
+  }
+
+  const body = {resumeObject, skillArray};
+
+  // send fetch or ajax request here
+
+
+});
